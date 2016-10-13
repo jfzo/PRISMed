6fcaf741da16aaac4633ef1610a8f95f149e311a
jfzo <juanzox@gmail.com>
2016-10-13 17:25:50 -0300

    wsdl interface added

--- src/prism_controller.py
+++ src/prism_controller.py
@@ -1,3 +1,16 @@
+import wsme
+from wsme import WSRoot
+from wsme.types import Enum, binary
+from wsme.types import UserType
+from wsme import WSRoot, expose, validate, signature
+
+
+#CONFIGURATION DIRECTIVES
+ip_address='10.100.45.123'
+bind_port='8080'
+temp_dir='/tmp'
+
+
 class SubjectCaptureUploadController:
     def __init__(self):
         pass
@@ -7,4 +20,59 @@ class SubjectCaptureUploadController:
 
     def save_model(self, ):
         pass
+
+
+
+#### WSDL Service Interface
+
+class PackageContent(object):
+    filename = unicode
+    content = unicode
+
+    def __repr__(self):
+        return "PackageContent(%s, %s)" % (
+            self.filename, self.content
+        )
+
+
+
+class FrontController(WSRoot):
+    '''
+    HOW TO USE THIS WSDL INTERFACE
+
+    import base64
+    with open("<PATH_TO_THE_CAPTURE_ZIP_FILE>/Ax_FSPGR_3D_7.zip",'rb') as zip_file:
+        encoded_string = base64.b64encode(zip_file.read())
+
+    url = 'http://10.100.45.123:8080/ws/api.wsdl'
+    client = Client(url, cache=None)
+    pc = client.service.create()
+
+    pc.filename = 'Ax_FSPGR_3D_7.zip'
+    pc.content = encoded_string
+    client.service.upload_packed_content( pc )
+    '''
+    @expose(PackageContent)
+    def create(self):
+        p = PackageContent()
+        p.filename = ''
+        p.content = ''
+        return p
+
+    @expose()
+    @validate(PackageContent)
+    def upload_packed_content(self, o):
+        logging.debug( "received "+o.filename )     
+
+        with open(temp_dir+'/'+o.filename, "wb") as fh:
+            fh.write(o.content.decode('base64'))
+
+        # delegate the content processing to the corresponding controller.
+        return len(o.content)
+
+
+
+
+
+
 
--- src/prism_util.py
+++ src/prism_util.py
@@ -1,10 +1,10 @@
 import zipfile as zf
 
 
-class StudyUnpacker:
+class SubjectCaptureUnpacker:
     def __init__(self):
         pass
-    def unpack_study(self, ):
+    def unpack_study(self, p0):
         pass
 
 
